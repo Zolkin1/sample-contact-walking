65f46f4d1c6417d526fc8fd2b3bd72311ba71089
Zachary <zach.olkin@gmail.com>
2024-08-08 17:00:18 -0700

    updated model to removed weird contact issues, debugging mpc

--- sample_contact_walking/include/sample_contact_walking/achilles_mpc.h
+++ sample_contact_walking/include/sample_contact_walking/achilles_mpc.h
@@ -1,3 +1,5 @@
+#include <visualization_msgs/msg/marker_array.hpp>
+
 #include "rclcpp/rclcpp.hpp"
 #include "obelisk_controller.h"
 
@@ -5,6 +7,7 @@
 
 namespace achilles {
     using torc::mpc::vectorx_t;
+    using torc::mpc::vector3_t;
 
     class AchillesController : public obelisk::ObeliskController<obelisk_control_msgs::msg::PDFeedForward, obelisk_estimator_msgs::msg::EstimatedState> {
         public:
@@ -12,32 +15,61 @@ namespace achilles {
         protected:
 
         private:
+            // Control functions
             obelisk_control_msgs::msg::PDFeedForward ComputeControl() override;
-            void UpdateXHat(const obelisk_estimator_msgs::msg::EstimatedState& msg) override;
-
             void ComputeMpc();
 
-            void ConvertEigenToStd(const vectorx_t& eig_vec, std::vector<double>& std_vec);
+            // State functions
+            void UpdateXHat(const obelisk_estimator_msgs::msg::EstimatedState& msg) override;
 
+            // Helpers
+            void ConvertEigenToStd(const vectorx_t& eig_vec, std::vector<double>& std_vec);
             vectorx_t ConvertControlToMujocoU(const vectorx_t& pos_target, const vectorx_t& vel_target, const vectorx_t& feed_forward);
+
+            // Viz
+            void PublishTrajViz(const torc::mpc::Trajectory& traj, const std::vector<std::string>& viz_frames);
+
+            // States
+            enum ControllerState {
+                SeekInitialCond,
+                Mpc,
+                NoOutput
+            };
+            void TransitionState(const ControllerState& new_state);
+            ControllerState GetState();
+
             // ---------- Member Variables ---------- //
             static constexpr int FLOATING_POS_SIZE = 7;
             static constexpr int FLOATING_VEL_SIZE = 6;
             static constexpr int POS_VARS = 3;
             static constexpr int QUAT_VARS = 4;
 
+            // Viz information
+            std::vector<std::string> viz_frames_;
+
             // State flags
             bool recieved_first_state_;
             bool first_mpc_computed_;
 
+            ControllerState ctrl_state_;
+
             // Mutexes
             std::mutex est_state_mut_;
             std::mutex traj_out_mut_;
+            std::mutex ctrl_state_mut_;
 
             // Estimated state vectors
             vectorx_t q_;        // position, quat (x, y, z, w), joints
             vectorx_t v_;
 
+            // Target vectors
+            vectorx_t q_target_;
+            vectorx_t v_target_;
+
+            vectorx_t q_ic_;
+            vectorx_t v_ic_;
+
+            // Trajectories
             torc::mpc::Trajectory traj_out_;
             torc::mpc::Trajectory traj_mpc_;
 
--- sample_contact_walking/src/achilles_mpc.cpp
+++ sample_contact_walking/src/achilles_mpc.cpp
@@ -1,56 +1,114 @@
+#include <vector>
+
+#include "obelisk_node.h"
 #include "obelisk_ros_utils.h"
 
 #include "sample_contact_walking/achilles_mpc.h"
+
+// TODO:
+//  - Verify that if I start on the trajectory that the MPC is well behaved
+//  - Allow user to set the initial condition in the yaml
+//  - Check for paths first relative to $SAMPLE_WALKING_ROOT then as a global path
+//  - Add ROS diagonstic messages: https://docs.foxglove.dev/docs/visualization/panels/diagnostics#diagnosticarray
+//  - Update ObeliskNode, line 788 (is msg obelsik to be capical "T")
+
 
 namespace achilles
 {
     AchillesController::AchillesController(const std::string& name) 
     : obelisk::ObeliskController<obelisk_control_msgs::msg::PDFeedForward, obelisk_estimator_msgs::msg::EstimatedState>(name), 
-        recieved_first_state_(false), first_mpc_computed_(false), traj_start_time_(0) {
+        recieved_first_state_(false), first_mpc_computed_(false), ctrl_state_(NoOutput), traj_start_time_(0) {
         // For now model the feet as point contacts
         contact_state_.contacts.emplace("left_ankle_pitch", torc::models::Contact(torc::models::PointContact, false));
         contact_state_.contacts.emplace("right_ankle_pitch", torc::models::Contact(torc::models::PointContact, false));
 
         this->RegisterObkTimer("timer_mpc_setting", "mpc_timer", std::bind(&AchillesController::ComputeMpc, this));
 
+
+        this->declare_parameter<std::string>("viz_pub_setting");
+        // RCLCPP_ERROR_STREAM(this->get_logger(), "viz pub settings: " << this->get_parameter("viz_pub_setting").as_string());
+        auto viz_pub = this->CreatePublisherFromConfigStr<visualization_msgs::msg::MarkerArray>(this->get_parameter("viz_pub_setting").as_string());
+        this->publishers_["viz_pub"] = std::make_shared<obelisk::internal::ObeliskPublisher<visualization_msgs::msg::MarkerArray>>(viz_pub);
+
         //  Update model
         this->declare_parameter<std::string>("urdf_path", "");
         std::filesystem::path urdf_path(this->get_parameter("urdf_path").as_string());
 
+        // Create model
         std::string model_name = name + "_model";
         model_ = std::make_unique<torc::models::FullOrderRigidBody>(model_name, urdf_path);
 
+        // Create and configure MPC
         mpc_ = std::make_unique<torc::mpc::FullOrderMpc>("achilles_mpc_obk", this->get_parameter("params_path").as_string(), urdf_path);
         RCLCPP_INFO_STREAM(this->get_logger(), "MPC Created...");
 
         mpc_->Configure();
         RCLCPP_INFO_STREAM(this->get_logger(), "MPC Configured...");
 
-        traj_out_.UpdateSizes(model_->GetConfigDim(), model_->GetVelDim(), model_->GetNumInputs(), mpc_->GetContactFrames(), mpc_->GetNumNodes());
+        // Make the feet always in contact for a standing gait
+        torc::mpc::ContactSchedule cs(mpc_->GetContactFrames());
+        cs.InsertContact("right_foot", 0, 1);
+        cs.InsertContact("left_foot", 0, 1);
+        mpc_->UpdateContactSchedule(cs);
 
-        vectorx_t q_neutral = model_->GetNeutralConfig();
-        q_neutral(2) += 0.321;
-        traj_out_.SetDefault(q_neutral);
+        // Setup q and v targets
+        q_target_.resize(model_->GetConfigDim());
+        q_target_ << 0, 0, 0.97,    // position
+                    0, 0, 0, 1,     // quaternion
+                    0, 0, -0.26,    // L hips joints
+                    0.65, -0.43,    // L knee, ankle
+                    0, 0, 0, 0,     // L shoulders and elbow
+                    0, 0, -0.26,    // R hip joints
+                    0.65, -0.43,    // R knee ankle
+                    0, 0, 0, 0;     // R shoulders and elbow
+
+        v_target_.resize(model_->GetVelDim());
+        v_target_ << 0, 0, 0,
+                    0, 0, 0,
+                    0, 0, 0,
+                    0, 0, 0, 
+                    0, 0, 0,
+                    0, 0, 0,
+                    0, 0, 0,
+                    0, 0, 0;
+        mpc_->SetConstantConfigTarget(q_target_);
+        mpc_->SetConstantVelTarget(v_target_);
+
+        // Set initial conditions
+        // TODO: Do this from yaml
+        q_ic_ = q_target_;
+        v_ic_ = v_target_;
+
+        // Setup swing trajectories
+
+        // Create default trajectory
+        traj_out_.UpdateSizes(model_->GetConfigDim(), model_->GetVelDim(), model_->GetNumInputs(), mpc_->GetContactFrames(), mpc_->GetNumNodes());
+        std::vector<double> dt(mpc_->GetNumNodes() - 1);
+        std::fill(dt.begin(), dt.end(), 0.02);
+        traj_out_.SetDtVector(dt);
+        traj_out_.SetDefault(q_target_);
         traj_mpc_ = traj_out_;
 
         mpc_->SetWarmStartTrajectory(traj_out_);
         RCLCPP_INFO_STREAM(this->get_logger(), "Warm start trajectory created...");
+        
+        // Compute an initial MPC at the initial condition
 
-        // TODO: Remove -- add a dummy contact schedule
-        torc::mpc::ContactSchedule cs(mpc_->GetContactFrames());
-        const double contact_time = 0.3;
-        double time = 0;
-        for (int i = 0; i < 3; i++) {
-            if (i % 2 != 0) {
-                cs.InsertContact("right_foot", time, time + contact_time);
-                cs.InsertContact("right_hand", time, time + contact_time);
-            } else {
-                cs.InsertContact("left_foot", time, time + contact_time);
-                cs.InsertContact("left_hand", time, time + contact_time);
-            }
-            time += contact_time;
-        }
-        mpc_->UpdateContactSchedule(cs);
+        // Go to initial condition
+        TransitionState(SeekInitialCond);
+
+        // Visualization information
+        this->declare_parameter<std::vector<std::string>>("viz_frames", {""});
+        viz_frames_ = this->get_parameter("viz_frames").as_string_array();
+        std::erase_if(viz_frames_, [this](std::string& frame)
+            {
+                if (this->model_->GetFrameIdx(frame) == -1) {
+                    RCLCPP_ERROR_STREAM(this->get_logger(), "Viz frame: " << frame << " is not found in the URDF! Ignoring!");
+                    return true;
+                }
+                RCLCPP_INFO_STREAM(this->get_logger(), "Viz frame: " << frame << " is being used.");
+                return false;
+            });
     }
 
     void AchillesController::UpdateXHat(const obelisk_estimator_msgs::msg::EstimatedState& msg) {
@@ -113,7 +171,10 @@ namespace achilles
 
             // Get the current time
             double time = this->get_clock()->now().seconds();
-            RCLCPP_INFO_STREAM(this->get_logger(), "Time: " << time);
+
+            // TODO: remove
+            q = q_target_;
+            v = v_target_;
 
             mpc_->Compute(q, v, traj_mpc_);
             {
@@ -124,8 +185,15 @@ namespace achilles
                 // Assign time time too
                 traj_start_time_ = time;
             }
-            RCLCPP_INFO_STREAM(this->get_logger(), "MPC Computation Completed!");
-            first_mpc_computed_ = true;
+            // RCLCPP_INFO_STREAM(this->get_logger(), "MPC Computation Completed!");
+            if (GetState() != Mpc) {
+                TransitionState(Mpc);
+            }
+
+            // TODO: If this is slow, then I need to move it
+            PublishTrajViz(traj_mpc_, viz_frames_);
+
+            mpc_->PrintStatistics();
         }
     }
 
@@ -134,36 +202,44 @@ namespace achilles
         //  setpoints from the trajectory.
 
         obelisk_control_msgs::msg::PDFeedForward msg;
-
-        if (first_mpc_computed_) {
+        if (ctrl_state_ != NoOutput) {
             RCLCPP_INFO_STREAM_ONCE(this->get_logger(), "Publishing first control.");
             {
-                // Get the traj mutex to protect it
-                std::lock_guard<std::mutex> lock(traj_out_mut_);
-
-                double time = this->get_clock()->now().seconds();
-                // TODO: Do I need to use nanoseconds?
-                double time_into_traj = time - traj_start_time_;
-                // RCLCPP_INFO_STREAM(this->get_logger(), "Time into traj: " << time_into_traj);
-
                 vectorx_t q, v, tau;
-                traj_out_.GetConfigInterp(time_into_traj, q);
-                traj_out_.GetVelocityInterp(time_into_traj, v);
-                traj_out_.GetTorqueInterp(time_into_traj, tau);
 
-                // --- TODO: Remove! -- This is a safety in case the interpolation time is too large
-                if (q.size() == 0) {
-                    q = model_->GetNeutralConfig();
-                }
+                if (ctrl_state_ == Mpc) {
+                    // Get the traj mutex to protect it
+                    std::lock_guard<std::mutex> lock(traj_out_mut_);
 
-                if (v.size() == 0) {
-                    v = torc::mpc::vectorx_t::Zero(model_->GetVelDim());
-                }
+                    double time = this->get_clock()->now().seconds();
+                    // TODO: Do I need to use nanoseconds?
+                    double time_into_traj = time - traj_start_time_;
+                    // RCLCPP_INFO_STREAM(this->get_logger(), "Time into traj: " << time_into_traj);
 
-                if (tau.size() == 0) {
-                    tau = torc::mpc::vectorx_t::Zero(model_->GetNumInputs());
+                    traj_out_.GetConfigInterp(time_into_traj, q);
+                    traj_out_.GetVelocityInterp(time_into_traj, v);
+                    traj_out_.GetTorqueInterp(time_into_traj, tau);
+
+                    // --- TODO: Remove! -- This is a safety in case the interpolation time is too large
+                    if (q.size() == 0) {
+                        // TODO: remove!
+                        // throw std::runtime_error("Ending controller!");
+                        q = traj_out_.GetConfiguration(traj_out_.GetNumNodes()-1);
+                    }
+
+                    if (v.size() == 0) {
+                        v = traj_out_.GetVelocity(traj_out_.GetNumNodes()-1);
+                    }
+
+                    if (tau.size() == 0) {
+                        tau = traj_out_.GetTau(traj_out_.GetNumNodes()-1);
+                    }
+                    // ---
+                } else if (ctrl_state_ == SeekInitialCond) {
+                    q = q_ic_;
+                    v = v_ic_;
+                    tau = vectorx_t::Zero(model_->GetNumInputs());
                 }
-                // ---
 
                 // Make the message
                 vectorx_t u_mujoco = ConvertControlToMujocoU(q.tail(model_->GetNumInputs()),
@@ -178,6 +254,7 @@ namespace achilles
             }
 
             this->GetPublisher<obelisk_control_msgs::msg::PDFeedForward>(this->ctrl_key_)->publish(msg);
+            
         }
 
         return msg;
@@ -194,6 +271,70 @@ namespace achilles
         vectorx_t u(pos_target.size() + vel_target.size() + feed_forward.size());
         u << pos_target, vel_target, feed_forward;
         return u;
+    }
+
+    void AchillesController::TransitionState(const ControllerState& new_state) {
+        std::lock_guard<std::mutex> lock(ctrl_state_mut_);
+
+        ctrl_state_ = new_state;
+        std::string new_state_str;
+
+        switch (new_state) {
+        case SeekInitialCond:
+            new_state_str = "Seek Initial Condition";
+            break;
+        case Mpc:
+            new_state_str = "MPC";
+        case NoOutput:
+            new_state_str = "No Output";
+        default:
+            break;
+        }
+
+        RCLCPP_INFO_STREAM(this->get_logger(), "Transitioning to state: " << new_state_str);
+    }
+
+    AchillesController::ControllerState AchillesController::GetState() {
+        std::lock_guard<std::mutex> lock(ctrl_state_mut_);
+        return ctrl_state_;
+    }
+
+    void AchillesController::PublishTrajViz(const torc::mpc::Trajectory& traj, const std::vector<std::string>& viz_frames) {
+        // Compute FK for each frame and add the point to a marker message
+        visualization_msgs::msg::MarkerArray msg;
+        msg.markers.resize(viz_frames.size());
+        for (int i = 0; i < viz_frames.size(); i++) {
+            msg.markers[i].type = visualization_msgs::msg::Marker::LINE_STRIP;
+            msg.markers[i].header.frame_id = "world";
+            msg.markers[i].header.stamp = this->now();
+            msg.markers[i].ns = "mpc_traj_viz";
+            msg.markers[i].id = i;
+            msg.markers[i].action = visualization_msgs::msg::Marker::MODIFY;
+
+            msg.markers[i].scale.x = 0.01; // Width of the line segments
+
+            msg.markers[i].color.a = 1.0; // Don't forget to set the alpha!
+            msg.markers[i].color.r = 0.0;
+            msg.markers[i].color.g = 1.0;
+            msg.markers[i].color.b = 0.0;
+        }
+
+
+        for (int node = 0; node < traj.GetNumNodes(); node++) {
+            model_->FirstOrderFK(traj.GetConfiguration(node));
+
+            for (int i = 0; i < viz_frames.size(); i++) {
+                vector3_t frame_pos = model_->GetFrameState(viz_frames[i], pinocchio::WORLD).placement.translation();
+                geometry_msgs::msg::Point point;
+                point.x = frame_pos(0);
+                point.y = frame_pos(1);
+                point.z = frame_pos(2);
+
+                msg.markers[i].points.emplace_back(point);
+            }
+        }
+
+        this->GetPublisher<visualization_msgs::msg::MarkerArray>("viz_pub")->publish(msg);
     }
 
 } // namespace achilles
