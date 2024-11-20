01776e3433b57d4c02e6bc03be28c98f60579237
Zachary <zach.olkin@gmail.com>
2024-11-19 16:05:27 -0800

    python node can now be brought up and listens to the estimated state. need changes in obelisk before I can publish these messages

--- contact_planning/contact_planning/contact_planner.py
+++ contact_planning/contact_planning/contact_planner.py
@@ -18,13 +18,14 @@ class ContactPlanner(ObeliskController):
     def __init__(self, node_name: str = "example_position_setpoint_controller") -> None:
         """Initialize the example position setpoint controller."""
         super().__init__(node_name)
-        self.declare_parameter("test_param", "default_value")
-        self.get_logger().info(f"test_param: {self.get_parameter('test_param').get_parameter_value().string_value}")
+        self.get_logger().info("INITIALIZING")
+        # self.declare_parameter("test_param", "default_value")
+        # self.get_logger().info(f"test_param: {self.get_parameter('test_param').get_parameter_value().string_value}")
 
     def on_configure(self, state: LifecycleState) -> TransitionCallbackReturn:
         """Configure the controller."""
         super().on_configure(state)
-        self.joint_pos = None
+        self.get_logger().info("CONFIGURING")
         return TransitionCallbackReturn.SUCCESS
 
     def update_x_hat(self, x_hat_msg: ObeliskEstimatorMsg) -> None:
@@ -33,9 +34,10 @@ class ContactPlanner(ObeliskController):
         Parameters:
             x_hat_msg: The Obelisk message containing the state estimate.
         """
-        pass  # do nothing
+        self.q_est = np.concatenate((x_hat_msg.q_base, x_hat_msg.q_joints))
+        # self.get_logger().info("Estiamted state: " + np.array2string(self.q_est))
 
-    def compute_control(self) -> ObeliskControlMsg:
+    def compute_control(self):
         """
         Returns:
             obelisk_control_msg: The control message.
@@ -43,24 +45,40 @@ class ContactPlanner(ObeliskController):
 
         # setting the message
         cs_msg = ContactSchedule()
-        cs_msg.header.stamp = self.get_clock().now()
-
-        # TODO: Change
-        cs_msg.contact_info.resize(2)
+        cs_msg.header.stamp = self.get_clock().now().to_msg()
 
         contact_info = ContactInfo()
 
         # For now I will hardcode all of these settings
         contact_frames = ["left_toe", "left_heel", "right_toe", "right_heel"]
 
-        for frame in contact_frames:
-            contact_info.contact_frame = frame
+        num_contacts = 3
 
-        self.obk_publishers["pub_ctrl"].publish(cs_msg)
-        return cs_msg  # type: ignore
+        for frame in contact_frames:
+            # Assign the frame
+            contact_info.robot_contact_frame = frame
+
+            # Assign the swing times
+            # TODO: Fill this in
+
+            # Assign the contact polytopes
+            # TODO: Change this
+            for i in range(num_contacts):
+                polytope = ContactPolytope()
+                polytope.a_mat = [1., 0., 0., 1.]
+                polytope.b_vec = [1., 1., -1., -1.]
+
+                contact_info.polytopes.append(polytope)
+
+            cs_msg.contact_info.append(contact_info)
+
+        # TODO: Need to update obelisk
+
+        # self.obk_publishers["pub_ctrl"].publish(cs_msg)
+        # self.get_logger().info("Publishing a contact schedule")
+        return cs_msg
     
 def main(args: Optional[List] = None):
-    print('Hi from contact_planning.')
     spin_obelisk(args, ContactPlanner, SingleThreadedExecutor)
 
 
